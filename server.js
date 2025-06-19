52d8ecba97ee45be8a223e5285323b07c92fbde1
Alna1104 <alnabchungath@gmail.com>
2025-06-19 17:59:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/enquiries", require("./routes/enquiries"));
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
