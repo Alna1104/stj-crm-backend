5fefb0601ee2d40b79782061e580e63455615f98
Alna1104 <alnabchungath@gmail.com>
2025-06-21 12:56:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/enquiries", require("./routes/enquiries"));
 
+
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
