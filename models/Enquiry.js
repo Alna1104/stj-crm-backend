54295b8a3a5c8b44eb143cee65051cf50e40f87d
Alna1104 <alnabchungath@gmail.com>
2025-06-21 12:55:30 +0530

    Update Enquiry.js
    Updated without upload button

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -1,22 +1,14 @@
 const mongoose = require("mongoose");
 
-const enquirySchema = new mongoose.Schema({
-  customerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Customer",
-    required: true
+const enquirySchema = new mongoose.Schema(
+  {
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", required: true },
+    description: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    type: { type: String, enum: ["Project", "Product"], required: true },
+    operation: { type: String, enum: ["Machining", "Laser Cutting"], required: true },
   },
-  description: String,
-  quantity: Number,
-  type: {
-    type: String,
-    enum: ["Project", "Product"]
-  },
-  operation: {
-    type: String,
-    enum: ["Machining", "Laser Cutting"]
-  },
-  fileUrl: String,
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Enquiry", enquirySchema);
