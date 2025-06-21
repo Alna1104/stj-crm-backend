34c8d4a046312a35bc720f8b49694cb0787edc3e
Alna1104 <alnabchungath@gmail.com>
2025-06-21 20:10:46 +0530

    Update enquiries.js

--- routes/enquiries.js
+++ routes/enquiries.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Enquiry = require("../models/Enquiry");
 const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const path = require("path");
 
 // Set up storage configuration
